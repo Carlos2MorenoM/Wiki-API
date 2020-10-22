044a204b4389114f00805be285d8f8c8517fb676
Carlos <c.morenomontesdeoca@gmail.com>
2020-10-22 08:06:49 +0200

    Initial files

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require ("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true
+}));
+app.use (express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useUnifiedTopology: true, useNewUrlParser: true});
+
+// SCHEMA
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+// MODEL
+const Article = mongoose.model("Article", articleSchema);
+
+//GET METHOD
+
+app.get("/articles", function(req,res){
+    Article.find(function(err, foundArticles){
+        if (!err){
+            res.send(foundArticles)
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000")
+});
